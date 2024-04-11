944ff44285b3c3d2208944d726a7db4eaa62e649
Abel Kabeya <abelkofficial@gmail.com>
2024-04-11 22:16:58 +0200

    Special Price start

--- customJs.js
+++ customJs.js
@@ -1,26 +1,17 @@
 $(document).ready(function() {
-    // Initialize the banner owl carousel
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots: true,
-        items: 1
-    });
+    setTimeout(function() {
+        var $grid = $('.grid').isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows'
+        });
+        console.log('Isotope grid initialized');
 
-    //Top Sale Owl Carousel
-    $("#top-sale .owl-carousel").owlCarousel({
-        loop: true,
-        nav: true,
-        dots: false,
-        responsive : {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000 : {
-                items: 5
-            }
-        }
-    });
+        $('.button-group').on('click', 'button', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+    }, 1000);
 
 });
+
+
